d36ce3ec664ec2ff280ddc5d205ec8e740757485
Martin Menzel <martin.menzel@zkb.ch>
2024-12-03 11:02:09 +0100

    d3

--- day2/main.py
+++ day2/main.py
@@ -1,41 +1,40 @@
+import re
+
 file = "data.txt"
 
 def p1():
-    safe = 0
     with open(file) as f:
-        for l in f:
-            nums = [int(i) for i in l.strip().split()]
-            try:
-                diff = (nums[1] - nums[0]) / (abs(nums[1] - nums[0]))
-            except ZeroDivisionError:
-                continue
+        s = "".join([l.strip() for l in f])
 
-            is_safe = all(0 < (num2 - num1) * diff < 4 for num1, num2 in zip(nums[:-1], nums[1:]))
-            if is_safe:
-                safe += 1
-    return safe
+    regex = re.compile("mul\((\d+,\d+)\)")
+    muls = [i.split(",") for i in regex.findall(s)]
+    return sum(int(i[0]) * int(i[1]) for i in muls)
 
 def p2():
-    safe = 0
     with open(file) as f:
-        for l in f:
-            nums = [int(i) for i in l.strip().split()]
-            is_safe = False
-            for index in range(len(nums)):
-                num_without = nums[:index] + nums[index + 1:]
-                try:
-                    diff = (num_without[1] - num_without[0]) / (abs(num_without[1] - num_without[0]))
-                except ZeroDivisionError:
-                    continue
+        s = "".join([l.strip() for l in f])
 
-                is_safe = all(0 < (num2 - num1) * diff < 4 for num1, num2 in zip(num_without[:-1], num_without[1:]))
-                if is_safe:
-                    break
+    rdos = re.compile("do\(\)")
+    rdonts = re.compile("don't\(\)")
+    rmuls = re.compile("mul\((\d+,\d+)\)")
 
-            if is_safe:
-                safe += 1
+    modifiers = []
+    modifiers += [(i.start(),True) for i in rdos.finditer(s)]
+    modifiers += [(i.start(), False) for i in rdonts.finditer(s)]
+    modifiers.sort(key=lambda a: a[0])
 
-    return safe
+    active = True
+    sum = 0
+    for i in rmuls.finditer(s):
+        while len(modifiers) > 0 and modifiers[0][0] < i.start():
+            mod = modifiers.pop(0)
+            active = mod[1]
+
+        if not active:
+            continue
+        m = i.group(1).split(",")
+        sum +=  int(m[0]) * int(m[1])
+    return sum
 
 print(p1())
 print(p2())
