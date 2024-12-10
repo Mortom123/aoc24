e0c55f1e8d95d62d1c055e0bbbf24c30a27ce50d
martin <martin@nitr.am>
2024-12-11 00:04:37 +0100

    d6 partial

--- day6/main.py
+++ day6/main.py
@@ -13,8 +13,6 @@ def p1():
     grid = Grid2D(lines[::-1])
     current_point = Point(*grid.find_all("^")[0])
 
-    print(grid)
-
     max_x, max_y = grid.size()
     visited = set()
     current_direction = Direction.EAST
@@ -23,10 +21,7 @@ def p1():
         visited.add(current_point)
         next_point = current_point + current_direction.point()
 
-        print(current_point, current_direction.point(), next_point)
-        print(grid[next_point.x, next_point.y])
-        while (grid[next_point.x, next_point.y] != "." and
-               grid[next_point.x, next_point.y] is not None):
+        while grid[next_point.x, next_point.y] == "#":
             current_direction = current_direction.left()
             next_point = current_point + current_direction.point()
 
@@ -34,7 +29,63 @@ def p1():
 
     return len(visited)
 
+def p2():
+    lines = []
+    with open(file) as f:
+        for l in f:
+            lines.append(list(l.strip()))
+
+    grid = Grid2D(lines[::-1])
+    current_point = Point(*grid.find_all("^")[0])
+
+    max_x, max_y = grid.size()
+    visited_list = []
+
+    current_direction = Direction.EAST
+
+    while 0 <= current_point.x < max_x and 0 <= current_point.y < max_y:
+        visited_list.append((current_point, current_direction))
+
+        next_point = current_point + current_direction.point()
+
+        while grid[next_point.x, next_point.y] == "#":
+            current_direction = current_direction.left()
+            next_point = current_point + current_direction.point()
+
+        current_point = next_point
+
+    visited = set(visited_list)
+
+    obstacles = 0
+    total = len(visited_list)
+    for i, (position, direction) in enumerate(visited_list):
+        print(f"{i}/{total}")
+        # Check if placing an obstacle at the next position would create a loop
+        obstacle_position = position + direction.point()
+        direction_with_obstacle = direction.left()
+        position_with_obstacle = position
+        visited_obstacle = visited.copy()
+        print(len(visited_obstacle))
+
+        while 0 <= position_with_obstacle.x < max_x and 0 <= position_with_obstacle.y < max_y:
+            if (position_with_obstacle, direction_with_obstacle) in visited_obstacle:
+                obstacles += 1
+                break
+            visited_obstacle.add((position_with_obstacle, direction_with_obstacle))
+            next_position_with_obstacle = position_with_obstacle + direction_with_obstacle.point()
+
+            while (grid[next_position_with_obstacle.x, next_position_with_obstacle.y] == "#"
+                   or next_position_with_obstacle == obstacle_position):
+                direction_with_obstacle = direction_with_obstacle.left()
+                next_position_with_obstacle = next_position_with_obstacle + direction_with_obstacle.point()
+            position_with_obstacle = next_position_with_obstacle
+
+    return obstacles
+
+
 print(p1())
+print(p2())
+
 
 
 
